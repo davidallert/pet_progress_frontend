0d9bada535137d164516ed1cc6eab63428472203
davidallert <david.allert@hotmail.com>
2025-09-17 15:10:39 +0200

    Continued working on the Timeline.

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import styles from "./page.module.css";
-import { useState, useContext } from "react";
+import { useEffect, useState, useContext } from "react";
 import PopupContext from '@/app/context/popup/context';
 
 export default function Timeline() {
@@ -11,20 +11,37 @@ export default function Timeline() {
   // Return an empty page, just displaying the header and footer.
   if (loading) return <main className={styles.main}>IMAGINE A SPINNER ICON</main>;
 
+  useEffect(() => {
+    const timelineBar = document.getElementById("timelineBar");
+    if (timelineBar) {
+      timelineBar.style.height = `${window.innerHeight}px`;
+    }
+  }, []);
+
   return (
+    // Do this instead: https://stephane-monnot.github.io/react-vertical-timeline/#/
+    // Easier than having to deal with spaces between lines etc.
     <main className={styles.main}>
-      <h1>Timeline</h1>
-      <section className={styles.timeline} aria-label="Timeline">
-        {/* <h2>Lily's birthday</h2> */}
-        {/* <h3>2023-12-24</h3> TODO: Make sure this doesn't cause issues with the size of styles.left */}
+      <section className={styles.timelineWrapper} aria-label="Timeline section">
+        <div id="timelineBar" className={styles.timelineBar}></div>
 
-        <div className={styles.vertical}></div>
-        <div className={styles.left}></div>
+        <section className={styles.timelineStart}>
+          <h1>Timeline</h1>
+          <section aria-label="First timeline event">
+            <h2>Lily's birthday</h2>
+            <h3>2023-12-24</h3>
+          </section>
+        </section>
 
-        <div className={styles.vertical}></div>
-        <div className={styles.right}></div>
+        <section className={`${styles.left} ${styles.event}`} aria-label="Timeline event">
+          <h3>Learned sit</h3>
+          <p>2024-03-10</p>
+        </section>
 
-        <div className={styles.vertical}></div>
+        <section className={`${styles.right} ${styles.event}`} aria-label="Timeline event">
+          <h3>Learned stay</h3>
+          <p>2024-03-22</p>
+        </section>
       </section>
     </main>
   );
