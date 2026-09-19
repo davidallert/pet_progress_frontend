327073befb7cc745b1823b9ea8d7505794c20b53
David <david.allert@hotmail.com>
2026-09-19 22:59:29 +0200

    Restructure code

--- src/app/hooks/useUserData.tsx
+++ src/app/hooks/useUserData.tsx
@@ -1,41 +1,48 @@
 import { useEffect, useState, useContext, Dispatch, SetStateAction } from "react";
 import axios from '../libraries/axios';
-import { AxiosError } from 'axios';
+import { isAxiosError } from 'axios';
 import type { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import type { PopupInput } from '@/app/context/popup/interface';
+
+interface UseUserDataOptions {
+  // petLimit?: number,
+  eventLimit?: number,
+}
+
+interface User {
+  id: number,
+  name: string,
+  email: string,
+  email_verified_at: null,
+  created_at: null,
+  updated_at: null
+}
+
+interface Pet {
+  id: number,
+  userId: number,
+  name: string,
+  imagePath: string,
+  birthday: string,
+  species: string,
+  breed: string,
+  events: Array<{
+    date: string,
+    id: number,
+    petId: number,
+    title: string,
+    description: string,
+    imagePath: string
+    type: string,
+  }>
+}
 
 export const useUserData = (
     router: AppRouterInstance,
     setLoading: Dispatch<SetStateAction<boolean>>,
-    setPopup: (popup: PopupInput) => void
+    setPopup: (popup: PopupInput) => void,
+    options: UseUserDataOptions
 ) => {
-  interface User {
-    id: number,
-    name: string,
-    email: string,
-    email_verified_at: null,
-    created_at: null,
-    updated_at: null
-  }
-
-  interface Pet {
-    id: number,
-    userId: number,
-    name: string,
-    imagePath: string,
-    birthday: string,
-    species: string,
-    breed: string,
-    events: Array<{
-      date: string,
-      id: number,
-      petId: number,
-      title: string,
-      description: string,
-      imagePath: string
-      type: string,
-    }>
-  }
 
   const [user, setUser] = useState<User | null>(null);
   const [pets, setPets] = useState<Array<Pet>>([]);
@@ -43,17 +50,21 @@ export const useUserData = (
   useEffect(() => {
     const getUser = async () => {
       try {
-        const response = await axios.get('/api/user/data');
-        console.log(response)
+        const response = await axios.get('/api/user/data', {
+          params: options
+        });
+
         setUser(response.data.user);
         setPets(response.data.user.pets);
-        setLoading(false);
       }
       catch (e) {
-        if (e instanceof(AxiosError)) {
-          setPopup({messages: [e.response?.data?.message + "."], type: 'error', isVisible: true})
+        if (isAxiosError(e)) {
+          setPopup({messages: [e.response?.data?.message + "."], type: 'error'});
+          router.replace('/');
         }
-        router.push('/');
+      }
+      finally {
+        setLoading(false);
       }
     }
     getUser()
