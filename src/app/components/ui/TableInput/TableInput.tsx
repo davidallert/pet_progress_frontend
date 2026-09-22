8f8ad1966f444cbdab305dbde9fa1a9c2bab90cd
David <david.allert@hotmail.com>
2026-09-22 15:02:16 +0200

    Create separate input component for table structures

---
+++ src/app/components/ui/TableInput/TableInput.tsx
@@ -0,0 +1,15 @@
+import styles from './TableInput.module.css'
+
+export default function TableInput(props: React.InputHTMLAttributes<HTMLInputElement>) {
+
+  if (props.type === "textarea") {
+    return <textarea rows="2" className={styles.input} {...(props as any)} />;
+  }
+
+  return (
+      <input
+        className={(props.type !== 'submit' ? `${styles.input}` : `${styles.input} ${styles.submit}`)}
+        {...props}
+      />
+  )
+}
